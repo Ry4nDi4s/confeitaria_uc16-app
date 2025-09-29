a8019ac8d00dc30558f51b7cf077dda1cb8c50bf
Ry4nDi4s <RyanDias52@outlook.com>
2025-09-29 16:29:38 -0300

    ...

--- src/componentes/Logo/index.tsx
+++ src/componentes/Logo/index.tsx
@@ -5,7 +5,7 @@ export default function Logo(){
     return(
         <>
         <span className="img">
-            <Image src="/images/logo.jpeg" alt="logo do mercadinho" fill style={{objectFit: "contain"}}/>
+            <Image src="/images/logo.jpeg" alt="Dani Bolos & Doces" fill style={{objectFit: "contain"}}/>
         </span>
         </>
     )
