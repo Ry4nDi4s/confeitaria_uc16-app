2ff73a331ea4d993292faaa653044152005c84c8
Ry4nDi4s <RyanDias52@outlook.com>
2025-10-03 15:24:40 -0300

    barra de pesquisa

--- src/Componentes/Menu/index.tsx
+++ src/Componentes/Menu/index.tsx
@@ -5,9 +5,11 @@ export default function Menu(){
     return(
         <>
         <nav className={styles.menu}>
+        <div id={styles.barraPesquisa}>
+        <input type="text" className={styles.campoPesquisa} placeholder="Buscar..."/>
+        <button className={styles.botaoPesquisa}>Pesquisar</button>
+        </div>
         <Link href="/">Sobre nós</Link>
-
-
         </nav>
         </>
     );
