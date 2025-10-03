8f82909f73b826e83aa3ac2a0fc3b1b6521785da
guilhermebertogo10 <guilhermebertogo123@gmail.com>
2025-10-03 16:37:36 -0300

    lupa

--- src/Componentes/Barra_Busca/index.tsx
+++ src/Componentes/Barra_Busca/index.tsx
@@ -1,12 +1,14 @@
-import styles from "./styles.module.css"
+import styles from "./styles.module.css";
 
-export default function Pesquisa(){
-    return(
-        <>
-        <div className={styles.barraPesquisa}>
-        <input type="text" className={styles.campoPesquisa} placeholder="Buscar..."/>
-        <button className={styles.botaoPesquisa}>Pesquisar</button>
-        </div>
-        </>
-    );
+export default function Pesquisa() {
+  return (
+    <div className={styles.barraPesquisa}>
+      <input
+        type="text"
+        className={styles.campoPesquisa}
+        placeholder="Buscar..."
+      />
+      <span className={styles.iconeLupa}>🔍</span>
+    </div>
+  );
 }
