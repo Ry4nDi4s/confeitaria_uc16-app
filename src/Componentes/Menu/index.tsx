05d3c9a562fe077df21d78f80b1c76051a655f7d
guilhermebertogo10 <guilhermebertogo123@gmail.com>
2025-10-03 14:52:42 -0300

    adiçao de menu

--- src/Componentes/Menu/index.tsx
+++ src/Componentes/Menu/index.tsx
@@ -1,8 +1,14 @@
-import "./styles.module.css";
+import Link from "next/link";
+import styles from "./styles.module.css";
 
 export default function Menu(){
     return(
         <>
+        <nav className={styles.menu}>
+        <Link href="/">Sobre nós</Link>
+
+
+        </nav>
         </>
     );
 }
