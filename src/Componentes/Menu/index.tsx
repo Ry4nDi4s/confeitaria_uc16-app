643efb836dd2767a8831cc1a29a5b8eee1ffafbb
Ry4nDi4s <RyanDias52@outlook.com>
2025-10-03 17:01:55 -0300

    ajuste de posição

--- src/Componentes/Menu/index.tsx
+++ src/Componentes/Menu/index.tsx
@@ -4,10 +4,8 @@ import styles from "./styles.module.css";
 export default function Menu(){
     return(
         <>
-        <nav className={styles.menu}>
-        <Link href="/">Sobre nós</Link>
-        <Link href="/contato" target="_blank">Contato</Link>
-        </nav>
+        <Link className={styles.menu} href="/">Sobre nós</Link>
+        <Link className={styles.menu} href="/contato" target="_blank">Contato</Link>
         </>
     );
 }
