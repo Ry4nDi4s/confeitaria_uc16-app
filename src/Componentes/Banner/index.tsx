b9e36db008b6ea2c695c8cc673a0a367d13ced44
Ry4nDi4s <RyanDias52@outlook.com>
2025-10-06 14:02:14 -0300

    adição de banner

---
+++ src/Componentes/Banner/index.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image"
+import styles from "./styles.module.css"
+
+export default function Logo(){
+    return(
+        <>
+        <span className={styles.image}>
+            <Image src="/images/banner/Banner - 1" alt="Dani Bolos & Doces" fill style={{objectFit: "contain"}}/>
+        </span>
+        </>
+    )
+}
---
+++ src/app/page_adm.tsx
@@ -0,0 +1,10 @@
+import "./page.css"
+import Header from "@/Componentes/Header";
+
+export default function Home() {
+    return(
+    <>
+    <Header/>
+    </>
+  );
+}
