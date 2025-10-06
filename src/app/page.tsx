b37509ca8d8752b392d87afdf2f69d631a5721de
Ry4nDi4s <RyanDias52@outlook.com>
2025-10-06 14:59:16 -0300

    adição do bootstrap e banner

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,14 @@
-import "./page.css"
+import Banner from "@/Componentes/Banner";
 import Header from "@/Componentes/Header";
+import styles from "@/app/page.module.css"
 
 export default function Home() {
     return(
     <>
+    <div className={styles.pagina}>
     <Header/>
-    
+    <Banner/>
+    </div>
     </>
   );
 }
