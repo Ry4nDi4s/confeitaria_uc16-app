06b8364c5ca2a55b1da7991f1e2c3e9b81583123
Ry4nDi4s <RyanDias52@outlook.com>
2025-10-06 14:03:10 -0300

    criando contato

--- src/Componentes/Banner/index.tsx
+++ src/Componentes/Banner/index.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image"
 import styles from "./styles.module.css"
 
-export default function Logo(){
+export default function Banner(){
     return(
         <>
         <span className={styles.image}>
