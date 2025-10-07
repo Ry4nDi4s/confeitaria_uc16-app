9172dc686e31a8ac8df952d0f7f5c4a3c9a72af9
Ry4nDi4s <RyanDias52@outlook.com>
2025-10-07 13:48:06 -0300

    Ajustando posição do contato

--- src/Componentes/Header2/index.tsx
+++ src/Componentes/Header2/index.tsx
@@ -1,12 +1,12 @@
-import Contato from "../Contato";
 import Links from "../Links";
+import Logo from "../Logo";
 import styles from "./styles.module.css";
 
 export default function Header2() {
     return(
         <>
             <header className={styles.header2}>
-                <Contato/>
+                <Logo/>
                 <Links/>
             </header>
         </>
