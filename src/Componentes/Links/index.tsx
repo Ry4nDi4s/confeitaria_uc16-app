be5c1c9d258cdf59fc13f6c85494d3f81925886c
Ry4nDi4s <RyanDias52@outlook.com>
2025-10-09 16:39:28 -0300

    Ajuste em contatos e logo

--- src/Componentes/Links/index.tsx
+++ src/Componentes/Links/index.tsx
@@ -7,17 +7,17 @@ export default function Links() {
     <div className={styles.whats}>
       <Link href="https://wa.me/5516999999999" className={styles.item} target="_blank">
         <FaWhatsapp className={styles.icon} />
-        <span>Fale por Whatsapp: (16) 99999-9999</span>
+        <span>Fale por Whatsapp: <br/> (16) 99999-9999</span>
       </Link>
 
       <Link href="https://instagram.com/dani_bolos_doces" className={styles.item} target="_blank">
         <FaInstagram className={styles.icon} />
-        <span>Fale pelo Instagram<br />dani_bolos_doces</span>
+        <span>Fale pelo Instagram<br/>dani_bolos_doces</span>
       </Link>
 
       <Link href="email:danibolos@hotmail.com" className={styles.item}>
-        <FaEnvelope className={styles.icon} />
-        <span>Fale pelo email<br />danibolos@hotmail.com</span>
+        <FaEnvelope className={styles.icon}/>
+        <span>Fale pelo email<br/>danibolos@hotmail.com</span>
       </Link>
     </div>
   );
