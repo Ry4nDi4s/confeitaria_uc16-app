e7a775f76d23bad5035115f562a9711a47df4333
Ryan Dias <RyanDias52@outlook.com>
2025-11-04 11:08:40 -0300

    ajuste NavBar

--- src/Componentes/public/Barra_Busca/index.tsx
+++ src/Componentes/public/Barra_Busca/index.tsx
@@ -12,7 +12,7 @@ function Buscar() {
               type="text"
               placeholder="Pesquisar"
               className="me-2"
-              style={{width:400}}
+              style={{width:550}}
             />
           </Col>
           <Col xxl="auto">
