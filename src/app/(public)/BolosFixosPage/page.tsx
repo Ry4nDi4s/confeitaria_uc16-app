ce951c5b908d0ff8e00ab558ac84465f5ed234a4
guilhermebertogo10 <guilhermebertogo123@gmail.com>
2025-11-05 15:11:56 -0300

    page de bolos fixos em andamento

--- src/app/(public)/BolosFixosPage/page.tsx
+++ src/app/(public)/BolosFixosPage/page.tsx
@@ -1,7 +1,34 @@
-export default function BolosFixoPage(){
+'use client'
+
+import Loading from "@/Componentes/public/Loading";
+import ProductList from "@/Componentes/admin/ProductList";
+import { AxiosResponse } from "axios";
+import { useEffect, useState } from "react";
+import api from "@/services/api";
+
+
+export default function BolosFixosPage() {
+  const [produtos, setProdutos] = useState([]);
+  const [isLoading, setIsLoading] = useState(true)
+  
+  function loadProdutos(){
+  setIsLoading(true);
+  api.get("/products")
+  .then(function (response: AxiosResponse){
+    setProdutos(response.data)
+  })
+  .catch(function (){
+    alert("Fail")
+  })
+  .finally(function(){
+    setIsLoading(false);
+  })};
+  
+  useEffect(loadProdutos, []);
     return(
-        <>
-    
-        </>
-    );
+    <>
+    {(isLoading) && (<Loading/>)}
+    <ProductList produtos={produtos}/>
+    </>
+  );
 }
