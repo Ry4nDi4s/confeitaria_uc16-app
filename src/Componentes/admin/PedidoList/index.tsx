47ff8956780f3505224ea6d7cd6ed90b7f513902
guilhermebertogo10 <guilhermebertogo123@gmail.com>
2026-04-07 17:13:54 -0300

    css

--- src/Componentes/admin/PedidoList/index.tsx
+++ src/Componentes/admin/PedidoList/index.tsx
@@ -1,7 +1,6 @@
-import styles from "./styles.module.css"
+import styles from "./styles.module.css";
 import PedidosCard from "@/Componentes/admin/PedidoCard";
 import Pedidos from "@/Model/TiposPedidos";
-
 
 type Props = {
     pedido: Pedidos[]
@@ -9,15 +8,10 @@ type Props = {
 
 export default function PedidoList({ pedido }: Props) {
     return (
-        <ul className={styles.pedido}>
-            {pedido.map(function (pedido: Pedidos) {
-                return (
-                        <PedidosCard
-                            key={pedido.id}
-                            pedido={pedido}
-                        />
-                )
-            })}
+        <ul className={styles.lista}>
+            {pedido.map((p) => (
+                <PedidosCard key={p.id} pedido={p} />
+            ))}
         </ul>
     )
 }
